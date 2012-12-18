baad866aad4d6de1aa31fd8713c776e13c28b39d
Andres Fernando Torti <torti.max@gmail.com>
2012-12-18 20:00:24 -0300

    Modificaciones del menu para el nuevo analizador logico basado en Fragments.

--- src/com/multiwork/andres/MainMenu.java
+++ src/com/multiwork/andres/MainMenu.java
@@ -24,7 +24,7 @@ public class MainMenu extends SherlockListActivity{
    
 	private static final boolean DEBUG = true;
 	private static final String[] ClassName = {"com.multiwork.andres.LCView", "com.multiwork.andres.FrecView",
-		"com.multiwork.andres.LogicAnalizerView", "com.roboticarm.andres.BrazoRobot",
+		"com.multiwork.andres.LogicAnalizerActivity", "com.roboticarm.andres.BrazoRobot",
 		"com.multiwork.andres.PruebaParser"};
 	private static String[] MenuNames = new String[ClassName.length];
 	private static ActionBar actionBar;
