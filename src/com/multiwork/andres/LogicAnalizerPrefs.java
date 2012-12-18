c594e90816c5442a844385d7a8f263ab8358c606
Andres Fernando Torti <torti.max@gmail.com>
2012-12-18 19:59:49 -0300

    Preferencias

--- src/com/multiwork/andres/LogicAnalizerPrefs.java
+++ src/com/multiwork/andres/LogicAnalizerPrefs.java
@@ -115,11 +115,11 @@ public class LogicAnalizerPrefs extends PreferenceActivity {
 		// Si cambio el SampleRate verifico que sea posible para cada baudio seleccionado
 		if(changedPreference.contains("sampleRate")) {
 			// Baudios
-			final long baudRate[] = new long[LogicAnalizerView.channelsNumber];
+			final long baudRate[] = new long[LogicAnalizerFragment.channelsNumber];
 			boolean state = false;
 			
 			// Compruebo para cada baudio si es posible un correcto muestreo
-			for(int n=0; n < LogicAnalizerView.channelsNumber; ++n) {
+			for(int n=0; n < LogicAnalizerFragment.channelsNumber; ++n) {
 				baudRate[n] = Long.decode(getPrefs.getString("BaudRate" + (n+1), "9600"));
 				Log.i("Preferences", "baudRate[" + n + "]: " + baudRate[n]);
 				if( ((1.0d/baudRate[n]) /  (1.0d/newValue)) < 3.0d) state = true;
