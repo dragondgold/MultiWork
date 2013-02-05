27c66fcef5536d18809436938619dfa952238c14
andres <andres@Andres-NT>
2013-02-05 20:33:05 -0300

    Update

--- src/com/multiwork/andres/ConfirmDialog.java
+++ src/com/multiwork/andres/ConfirmDialog.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
