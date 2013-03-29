8b2124b5ebaf48d1e227746c587dcda67f864528
andres <andres@Andres-NT>
2013-03-29 19:33:00 -0300

    Modificaciones para ser el Fragment por defecto

--- src/com/protocolanalyzer/andres/LogicAnalizerListFragment.java
+++ src/com/protocolanalyzer/andres/LogicAnalizerListFragment.java
@@ -3,15 +3,13 @@ package com.protocolanalyzer.andres;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
 import com.multiwork.andres.R;
 import com.protocolanalyzer.api.andres.LogicData;
 import com.protocolanalyzer.api.andres.LogicData.Protocol;
 
 import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -32,19 +30,10 @@ public class LogicAnalizerListFragment extends SherlockFragment implements OnDat
 	private static TextView mRawDataTitle[] = new TextView[LogicAnalizerActivity.channelsNumber];
 	private static View v;
 	
-	private static LogicData[] logicData;
-	private static ProgressDialog dialog;
-	
-	public LogicAnalizerListFragment(){
-		
-	}
-	
-	/** 
-	 * Constructor
-	 * @param mLogicData datos iniciales que desean mostrarse
-	 */
-	public LogicAnalizerListFragment(LogicData[] mLogicData) {
-		logicData = mLogicData;
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		menu.findItem(R.id.listLogic).setIcon(R.drawable.chart2);
+		super.onPrepareOptionsMenu(menu);
 	}
 	
 	@Override
@@ -59,11 +48,6 @@ public class LogicAnalizerListFragment extends SherlockFragment implements OnDat
 		mActionBar.setDisplayHomeAsUpEnabled(true);					// El icono de la aplicacion funciona como boton HOME
 		mActionBar.setTitle(getString(R.string.AnalyzerName)) ;		// Nombre
         this.setHasOptionsMenu(true);
-     	
-		// Muestro un diálogo de progreso indeterminado mientras se procesan los datos
-		dialog = ProgressDialog.show(mActivity, mActivity.getString(R.string.AnalyzerDialogLoading),
-				mActivity.getString(R.string.PleaseWait), true);
-     	
 	}
 
 	@Override
@@ -91,40 +75,9 @@ public class LogicAnalizerListFragment extends SherlockFragment implements OnDat
 				return true;
 			}
 		});
-
-		// Creo un Handler para modificar el UI de forma tal que si hay datos, no se "tilde" el UI hasta que se
-		// muestre el texto, sino que se va escribiendo una vez dentro y no provoca tildes en el UI.
-		handler.sendEmptyMessage(0);
 		
 		return v;
 	}
-	
-	private static final Handler handler = new Handler(){
-	    @Override
-	    public void handleMessage(Message msg) {
-			// Si hay datos ya tomados los muestro ahora
-			if(logicData[0] != null){
-				for(int n=0; n < mRawData.length; ++n) mRawData[n].setText("");
-				for(int n=0; n < mRawData.length; ++n){
-					if(logicData[n].getProtocol() != Protocol.CLOCK){
-						mRawDataTitle[n].setText( Html.fromHtml("<u>" + mActivity.getString(R.string.AnalyzerChannel) + " " + (n+1) 
-								+ " - " + logicData[n].getProtocol().toString() + "</u>") );
-						
-						for(int i=0; i < logicData[n].getStringCount(); ++i){
-							// Con código HTML se puede aplicar propiedades de texto a ciertas partes unicamente
-							// http://stackoverflow.com/questions/1529068/is-it-possible-to-have-multiple-styles-inside-a-textview
-							mRawData[n].append( Html.fromHtml("<b><font color=#ff0000>" +
-									logicData[n].getString(i) + "</font></b>"  
-									+ "\t --> " + String.format("%.2f", (logicData[n].getPositionAt(i)[0]*1000000))
-									+ "uS<br/>") );
-						}
-					}
-				}
-			}
-			dialog.dismiss();
-			dialog = null;
-	    }
-	};
 	
 	@Override
 	public void onResume() {
