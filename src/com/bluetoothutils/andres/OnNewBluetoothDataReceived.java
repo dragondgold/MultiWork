abe56097bf6bff8fd5ac13a398e61632319fab3c
andres <andres@Andres-NT>
2013-04-24 19:33:16 -0300

    Agregada documentación sobre el Thread sobre el cual se ejecuta la interface.

--- src/com/bluetoothutils/andres/OnNewBluetoothDataReceived.java
+++ src/com/bluetoothutils/andres/OnNewBluetoothDataReceived.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 public interface OnNewBluetoothDataReceived {
 
 	/**
+	 * Esta interface se ejecuta en un Thread diferente para no bloquear el UI al recibir los datos por
+	 * lo que si se desea modificar la UI debe hacerse desde el Thread correspondiente
 	 * @param mBTIn InputStream del Bluetooth
 	 * @param mBTOut OutputStream del Bluetooth
 	 * @return true si se continua el listener, false de otro modo
